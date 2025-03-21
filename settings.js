cafc9b425a9ccfaca074bb1b2d52b0da5a7b1c2f
P-3-4 <amirpea4444@gmail.com>
2025-03-21 06:28:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "P34's Bot",
+  botName: "Knightbot",
   botOwner: 'P34', // Your name
   ownerNumber: '2348149404648', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.1",
+  version: "1.0.0",
 };
 
 module.exports = settings;
